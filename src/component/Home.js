2b666e0484dddf7c86249d2d1ed5927837fb2d53
manik452 <manik452@yahoo.com>
2022-09-03 21:05:05 +0600

    working on Long List

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { CartState } from '../context/Contex'
 import Filters from './Filters';
 import SingleProduct from './SingleProduct';
+import ReactPaginate from 'react-paginate';
+import { faker } from '@faker-js/faker';
+
 
 const Home = () => {
     const { state: { products },
@@ -34,14 +37,62 @@ const Home = () => {
         }
         return sortedProducts;
     }
+    /*new Array(1000).fill().map((value, index) => (({
+        id: index,
+        title: faker.lorem.words(5),
+        body: faker.lorem.sentences(8)
+    })))*/
+    const [pagination, setPagination] = useState({
+        data: transformProducts(),
+        offset: 0,
+        numberPerPage: 9,
+        pageCount: 0,
+        currentData: []
+    });
+    useEffect(() => {
+        setPagination((prevState) => ({
+            ...prevState,
+            data: transformProducts(),
+            pageCount: prevState.data.length / prevState.numberPerPage,
+            currentData: prevState.data.slice(pagination.offset, pagination.offset + pagination.numberPerPage)
+        }))
+    }, [pagination.numberPerPage, pagination.offset, pagination.data])
+    const handlePageClick = event => {
+        const selected = event.selected;
+        const offset = selected * pagination.numberPerPage
+        setPagination({ ...pagination, offset })
+    }
+
     return (
         <div className="home">
             <Filters />
             <div className="productContainer">
-                {transformProducts().map((prod) => {
+                {/*{transformProducts().map((prod) => {
                     return <SingleProduct prod={prod} />
-                })}
+                })}*/}
+                {pagination.currentData && pagination.currentData.map(((item, index) => (
+                   /* transformProducts().map((prod) => {
+                        return <SingleProduct prod={prod} />
+                    })*/
+                    
+                    <SingleProduct prod={item} />
+                )))
+                }
+                <div>
+                <ReactPaginate 
+                    previousLabel={'previous'}
+                    nextLabel={'next'}
+                    breakLabel={'...'}
+                    pageCount={pagination.pageCount}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={handlePageClick}
+                    containerClassName={'pagination'}
+                    activeClassName={'active'}
+                    />
+                </div>
             </div>
+            
         </div>
     )
 }
